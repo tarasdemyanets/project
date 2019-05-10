a9c4e0998d29ec7fad616cba9c8474b9bc416ddf
tarasdemyanets <tarasdemyanets1803@gmail.com>
2019-05-10 11:09:01 +0300

    Step 2 completed

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,19 @@
+class ReviewValidator < ActiveModel::Validator
+  def validate(record)
+    if record.reviewable_type == 'Item'
+      result = Booking.joins(:user, item: :owner).where(user: record.reviewer, items: { id: item_id })
+      record.errors[:base] << 'You newer booked this item' if result.empty?
+    else
+      result = Booking.joins(:item).where(user: record.reviewer, items: { owner: record.reviewable_id })
+      record.errors[:base] << 'You newer booked items of this user' if result.empty?
+    end
+  end
+end
 class Review < ApplicationRecord
   belongs_to :reviewable, polymorphic: true
   belongs_to :reviewer, class_name: 'User'
+  validates_with ReviewValidator
   scope :all_item, ->(user_id) do
-    includes(:reviewable).where(reviewable_type: 'Item')
+    joins('INNER JOIN items on items.id=reviews.reviewable_id').where(reviewable_type: 'Item', items: { owner_id: user_id })
   end
 end
